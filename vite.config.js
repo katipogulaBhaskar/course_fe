dcb47c21293e4010ba5f0fc1787426d46532d5ed
katipogulaBhaskar <katipogulabhaskar1234@gmail.com>
2025-01-10 20:24:47 +0530

    Fix Vite configuration and update Netlify settings

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensures output goes to the "dist" directory
+    assetsDir: 'assets', // Optional: Customize where assets go inside dist
+  }
 })
